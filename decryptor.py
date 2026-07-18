64a65da1c157a548e7f66877aaf1b22fbb748106
bowntowr22 <luke.francks@gmail.com>
2026-07-18 17:38:50 -0400

    Create decryptor.py

---
+++ decryptor.py
@@ -0,0 +1,11 @@
+# This project was ENTIRELY human coded by bowntowr on GitHub :) https://github.com/bowntowr
+# This is one of my first projects without vibecoding so there may be bugs!
+# This is the decryptor script to accompany the encryptor.py file, it will decrypt the string you give it and output it to the terminal
+
+# Importing necesary stuff
+
+# Asks the user what they want to decrypt
+to_decrypt = input("Enter the string you would like to decrypt: ")
+
+# Waits to exit so the user can see the output (otherwise the terminal will instantly close on Windows, linux is fine tho cuz linux is epic)
+input("\nPress enter to exit...")

f74457bfc32e7078f14df56f79b0cada2fd64f1e
bowntowr22 <luke.francks@gmail.com>
2026-07-18 17:38:47 -0400

    Update encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -24,10 +24,9 @@ encrypted = ""
 
 # Loop to encrypt inputted string
 for m_char, k_char in zip(to_encrypt, key):
-    # Patched to wrap around the legnth of ALLOWED_CHARS (62) instead of 256
-    combined = (ord(m_char) + ord(k_char)) % len(ALLOWED_CHARS)
-    # Patched to pull the character directly from the allowed list
-    encrypted += ALLOWED_CHARS[combined]
+    combined = (ord(m_char) + ord(k_char)) % 256
+
+encrypted += chr(combined)
 
 # Prints finalized encrypted string and key
 print("Encrypted string:", encrypted)
